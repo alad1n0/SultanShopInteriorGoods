1cf6d3f459391972221467fa32c74d2f12b1197c
alad1n0 <alad1nfaceit.al@gmail.com>
2024-03-14 19:32:53 +0200

    first commit

---
+++ src/components/About/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './_StartPage.scss';
+
+function About() {
+  return (
+    <h4>Головна</h4>
+  );
+}
+
+export default About;
---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './_About.scss';
+
+function About() {
+  return (
+    <h4>Головна</h4>
+  );
+}
+
+export default About;
